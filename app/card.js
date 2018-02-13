7baf249511eb824af00f910cd5e3e61937e5e454
WilliamDann <wcdann1@gmail.com>
2018-02-12 20:12:16 -0800

    Added Saving System

--- app/card.js
+++ app/card.js
@@ -1,5 +1,27 @@
+var fs = require('fs');
+
 class Card {
-    constructor (text) {
+    constructor (text, saveID=null) {
         this.text = text;
+        this.saveID = saveID;
     }
-}
+    
+    // Save the card
+    save(location) {
+        fs.writeFile(location, JSON.stringify(this), () => {});
+    }
+
+    // Load the card
+    static load(location, callback) {
+        fs.readFile(location, (err, data) => {
+            if (err) throw err;
+            var data = JSON.parse(data);
+            callback(new Card(data.text, data.saveID));
+        });
+    }
+
+    // Delete the card
+    delete(location, callback) {
+        fs.unlink(location, () => { callback(); })
+    }
+} module.exports = Card;
--- index.js
+++ index.js
@@ -2,9 +2,24 @@ const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
 const ipcMain = require('electron').ipcMain;
+var fs = require('fs');
+const Card = require('./app/card.js');
 let win
 
-function createPaper (text="Press tab to enter edit mode") {
+// Load all the papers in windows
+function loadPapers() {
+  // Save files
+  fs.readdir(path.join(__dirname, 'app/save'), (err, files) => {
+    if (err) { throw err; }
+    files.forEach(file => {
+      Card.load(path.join(path.join(__dirname, 'app/save'), file), paper => {
+        createPaper(paper.text, paper.saveID);
+      });
+    });
+  }); 
+}
+
+function createPaper (text="Press tab to enter edit mode", saveID=null) {
   win = new BrowserWindow({width: 306, height: 396, frame: false})
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'app/html/index.html'),
@@ -12,9 +27,10 @@ function createPaper (text="Press tab to enter edit mode") {
     slashes: true
   }))
 
+  // win.toggleDevTools();
   // Tell the notecard who it is
   win.webContents.on('did-finish-load', () => {
-    win.webContents.send("identity-assign", "Press Tab to enter edit mode")
+    win.webContents.send("identity-assign", {text:text,saveID:saveID})
   })
 
   win.on('closed', () => {
@@ -22,7 +38,7 @@ function createPaper (text="Press tab to enter edit mode") {
   })
 }
 
-app.on('ready', () => { createPaper() })
+app.on('ready', () => { loadPapers() })
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
